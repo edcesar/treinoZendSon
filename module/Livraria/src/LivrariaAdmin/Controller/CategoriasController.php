80f2a99af275b1c7e8b2445be4720f720d3532c8
Ed Cesar <edcesar@edcesar.com>
2017-02-10 01:08:53 +0000

    criado controller categoria

---
+++ module/Livraria/src/LivrariaAdmin/Controller/CategoriasController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace LivrariaAdmin\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+	/**
+	 * @var EntityManager
+	 */ 
+	protected $em;
+
+    public function indexAction()
+    {
+        $list = $this->getEm()
+        	->getRepository('Livraria\Entity\Categoria')
+        	->findAll();
+        
+        return new ViewModel(['data' => $list]);
+    }
+
+    protected function getEm()
+    {
+    	if (null === $this->em) {
+    		$this->em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+    	}
+    	return $this->em;
+    }
+
+
+}
+
