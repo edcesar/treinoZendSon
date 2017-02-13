4b4e95a171f97922d57d5bbfa09b9b7defb4e307
Ed Cesar <edcesar@edcesar.com>
2017-02-13 15:33:58 +0000

    mapeamento entidade livro ok

--- module/Livraria/src/Livraria/Entity/Livro.php
+++ module/Livraria/src/Livraria/Entity/Livro.php
@@ -30,7 +30,89 @@ class Livro
 	 * @ORM\JoinColumn(name="categoria_id", referencedColumnName="id")
 	 */ 
 	protected $categoria;
+
+	/**
+	 * @ORM\Column(type="text")
+	 * @var string
+	 */ 
 	protected $autor;
+
+	/**
+	 * @ORM\Column(type="text")
+	 * @var string
+	 */ 
 	protected $isbn;
+
+	/**
+	 * @ORM\Column(type="float")
+	 * @var float
+	 */ 
 	protected $valor;
+
+	public function __construct($options = null)
+	{
+		Configurator::configure($this, $options);
+	}
+
+	public function getId()
+	{
+		return $this->id;
+	}
+	public function getNome()
+	{
+		return $this->nome;
+	}
+	public function getCategoria()
+	{
+		return $this->categoria;
+	}
+	public function getAutor()
+	{
+		return $this->autor;
+	}
+	public function getIsbn()
+	{
+		return $this->isbn;
+	}
+	public function getValor()
+	{
+		return $this->valor;
+	}
+
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
+	public function setNome($nome)
+	{
+		$this->nome = $nome;
+	}
+	public function setCategoria($categoria)
+	{
+		$this->categoria = $categoria;
+	}
+	public function setAutor($autor)
+	{
+		$this->autor = $autor;
+	}
+	public function setIsbn($isbn)
+	{
+		$this->isbn = $isbn;
+	}
+	public function setValor($valor)
+	{
+		$this->valor = $valor;
+	}
+
+	public function toArray()
+	{
+		return [
+			'id' => $this->getId(),
+			'nome' => $this->getNome(),
+			'autor' => $this->getAutor(),
+			'isbn' => $this->getIsbn(),
+			'valor' => $this->getValor,
+			'categoria' => $this->getCategoria(),
+		];
+	}
 }
